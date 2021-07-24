8b317b3038f8489544c1592c27c9671e17957b94
Peter <wjdgustjr0554@gmail.com>
2021-07-24 21:54:22 +0900

    Running some diagnostics tests

--- server/utils/s3.js
+++ server/utils/s3.js
@@ -4,11 +4,13 @@ const S3 = require("aws-sdk/clients/s3");
 const fs = require("fs");
 
 const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION || process.env.AWS_REGION_MYAPP;
+const region = process.env.AWS_REGION_MYAPP || process.env.AWS_BUCKET_REGION;
 const accessKeyId =
-  process.env.AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID_MYAPP;
+  process.env.AWS_ACCESS_KEY_ID_MYAPP || process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey =
-  process.env.AWS_SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY_MYAPP;
+  process.env.AWS_SECRET_ACCESS_KEY_MYAPP || process.env.AWS_SECRET_ACCESS_KEY;
+
+console.log(bucketName, region, accessKeyId, secretAccessKey);
 
 const s3 = new S3({
   region,
