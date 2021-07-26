ddad0a43cf6b0878c9c01d192c2afd331fa59e85
Peter <wjdgustjr0554@gmail.com>
2021-07-26 22:29:05 +0900

    Adjustment for local development

--- server/db.js
+++ server/db.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
+  user: process.env.PG_USER || "postgres",
+  password: process.env.PG_PASSWORD || "raspberry",
+  host: process.env.PG_HOST || "localhost",
   port: 5432,
-  database: process.env.PG_DATABASE,
-  ssl: { rejectUnauthorized: false },
+  database: process.env.PG_DATABASE || "studius",
+  ssl: process.env.PG_USER ? { rejectUnauthorized: false } : undefined,
 });
 
 module.exports = pool;
