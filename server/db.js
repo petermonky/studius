0b630995fff0a033476a50aa5d8ccb5113afff9b
petermonky <wjdgustjr0554@gmail.com>
2021-07-28 17:09:32 +0900

    Retrack db.js

---
+++ server/db.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: 5432,
+  database: process.env.PG_DATABASE,
+  ssl:
+    process.env.PG_HOST === "localhost"
+      ? { rejectUnauthorized: false }
+      : undefined,
+});
+
+module.exports = pool;
